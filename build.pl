d17c16b51ff7cfd4e85b54452a1c3fb5adbbf2db
papix <mail@papix.net>
2015-07-04 12:48:35 +0900

    srcディレクトリに移動させた

--- build.pl
+++ build.pl
@@ -7,7 +7,7 @@ use Path::Tiny;
 
 my @slides = qw/ intro.md use.md scalar.md stdin-calc.md if-else.md logical-operator.md array-for.md outro.md /;
 my $slide = join("\n---\n", map {
-    my $str = path($_)->slurp_utf8;
+    my $str = path("src/$_")->slurp_utf8;
     $str =~ s/\n(##?)/\n___\n$1/g;
     $str
 } @slides);
